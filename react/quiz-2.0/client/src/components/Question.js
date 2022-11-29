e32e720ce4f106e198ce4f7a9bb7b8a0a0dad082
Josetruck <olemiluis@gmail.com>
2022-11-29 17:10:26 +0100

    tarde guapa

--- react/quiz-2.0/client/src/components/Question.js
+++ react/quiz-2.0/client/src/components/Question.js
@@ -1,7 +1,11 @@
 
 import React, { useState, useEffect } from "react";
 import '../App.css'
-
+import Popup from "./Popup";
+import useSound from "use-sound"
+import sndOk from "../audios/sndOk.mp3"
+import sndNo from "../audios/sndNo.mp3"
+import sndLoad from "../audios/sndLoad.mp3"
 
 export default function Question() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
@@ -11,6 +15,12 @@ export default function Question() {
   const [user, setUser] = useState("")
   const [storedScore, setStoredScore] = useState(false)
   const [top10, setTop10] = useState("")
+  const [popup, setPopup] = useState("")
+
+
+  const [playsndOk] = useSound(sndOk);
+  const [playsndNo] = useSound(sndNo);
+  const [playsndLoad] = useSound(sndLoad);
 
 
 
@@ -27,10 +37,15 @@ export default function Question() {
         localStorage.setItem("answers", JSON.stringify(answers))
       })
     setCurrentQuestion(0);
+    playsndLoad();
     setCounter(0);
     setScore(0)
-    setGameState("game");
-    setStoredScore(false)
+    setGameState("loading")
+    setTimeout(() => {
+      setGameState("game");
+      setStoredScore(false);
+    }, 8000)
+
   }
   var answers
   var questions
@@ -52,9 +67,17 @@ export default function Question() {
     if (currentQuestion < 10) {
       if (answers[currentQuestion][e] === correct_answer[currentQuestion]) {
         setScore(score + 1)
+        setPopup("ok")
+        playsndOk();
+      } else {
+        setPopup("no")
+        playsndNo();
       }
-      setCurrentQuestion(currentQuestion + 1)
-      setCounter(30)
+      setTimeout(() => {
+        setCurrentQuestion(currentQuestion + 1)
+        setCounter(30)
+        setPopup("")
+      }, 2500)
     }
   }
 
@@ -64,17 +87,17 @@ export default function Question() {
       setCurrentQuestion(currentQuestion + 1)
     }
   })
-  useEffect(()=>{
+  useEffect(() => {
     var stats = localStorage.storeScore
     if (stats) {
       stats = JSON.parse(stats)
       console.log(stats)
-      var ordered = stats.sort(function(a, b){return b.score-a.score})
+      var ordered = stats.sort(function (a, b) { return b.score - a.score })
       console.log(ordered)
-      var top10 = ordered.slice(0,10);
+      var top10 = ordered.slice(0, 10);
     }
     setTop10(top10)
-  },[storedScore])
+  }, [storedScore])
   useEffect(() => {
     if (currentQuestion > 9) {
       setGameState("finished")
@@ -120,47 +143,66 @@ export default function Question() {
   }
 
   return (
-    <div className="question">
+    <div className="game">
+      {popup === "ok"
+        ? <Popup value="ok" /> : null}
+      {popup === "no"
+        ? <Popup value="no" /> : null}
+      {gameState === "loading"
+        ? <Popup value="loading" /> : null}
       {gameState === "home"
-        ? <div>
+        ? <div className="home">
+          <h1>Hackers Quiz</h1>
+          <h3>Questions computer science</h3>
           <button onClick={() => newGame()}>Click Aqui para empezar</button>
         </div>
         : null}
       {gameState === "game" && currentQuestion < 10 && answers
-        ? <div>
-          <p>Timer: {counter} segs ////  Current Question: {currentQuestion}  //// Score: {score}/10</p>
-          <h2 dangerouslySetInnerHTML={{ __html: questions[currentQuestion] }}></h2>
-          <div className="answers">
-            <button onClick={() => check(0)} dangerouslySetInnerHTML={{ __html: answers[currentQuestion][0] }}></button>
-            <button onClick={() => check(1)} dangerouslySetInnerHTML={{ __html: answers[currentQuestion][1] }}></button>
-            <button onClick={() => check(2)} dangerouslySetInnerHTML={{ __html: answers[currentQuestion][2] }}></button>
-            <button onClick={() => check(3)} dangerouslySetInnerHTML={{ __html: answers[currentQuestion][3] }}></button>
+        ? <div className="page">
+          <div className="header">
+            <h2>Hacker's Quiz</h2>
+            <button onClick={() => setGameState("home")}>Finish Game</button>
+          </div>
+          <div className="card">
+            <p className="lineaH">---------------------------------------------------------------------</p>
+            <div className="upper"><p> Timer: {counter} segs</p> <p> Current Question: {currentQuestion}</p> <p> Score: {score}/10</p></div>
+            <p className="lineaH">---------------------------------------------------------------------</p>
+            <h2 dangerouslySetInnerHTML={{ __html: questions[currentQuestion] }} className="question"></h2>
+            <div className="answers">
+              <button className="A" onClick={() => check(0)} dangerouslySetInnerHTML={{ __html: answers[currentQuestion][0] }}></button>
+              <button className="B" onClick={() => check(1)} dangerouslySetInnerHTML={{ __html: answers[currentQuestion][1] }}></button>
+              <button className="C" onClick={() => check(2)} dangerouslySetInnerHTML={{ __html: answers[currentQuestion][2] }}></button>
+              <button className="D" onClick={() => check(3)} dangerouslySetInnerHTML={{ __html: answers[currentQuestion][3] }}></button>
+            </div>
           </div>
         </div>
         : null}
       {gameState === "finished"
         ? <div>
           <h1>Game Over</h1>
-          <p>Tu resultado en esta partida es de {score}.</p>
-          <button onClick={() => resetGame()}>Reiniciar Juego</button>
+          <p>Your result: {score}/10</p>
+          <button onClick={() => resetGame()}>Reset Game</button>
+          <button onClick={() => setGameState("home")}>Home page</button>
         </div>
         : null}
       {!storedScore && gameState === "finished"
-        ? <div><p>Introduce tu nombre para guardar la partida</p>
+        ? <div><p>Insert your name: </p>
           <input type="text" onChange={(e) => setUser(e.target.value)} />
           <button onClick={() => saveScore()}>Guardar</button></div>
         : null}
-      { gameState === "finished"
-      ?     <div className="scoretable">
-        <h2>Top 10 Players</h2>
-      <div className="scorerow"><h3>Name</h3><h3>Score</h3><h3>Date</h3></div>
-      </div>:null}
-        {gameState === "finished"
-          ?
-          top10.map((e, i) => {
-            return <div className="scorerow"><h3>{e.user}</h3><p>{e.score}</p><p>{e.date}</p></div>
-          })
-          : null}
+      {gameState === "finished"
+        ? <div className="scoretable">
+          <h2 className="top10title">Top 10 Players</h2>
+          <p className="lineaH">---------------------------------------------------------------------</p>
+          <div className="scorerow"><h3>Name</h3><h3>Score</h3><h3>Date</h3></div>
+          <p className="lineaH">---------------------------------------------------------------------</p>
+        </div> : null}
+      {gameState === "finished"
+        ?
+        top10.map((e, i) => {
+          return <div className="scorerow"><h3>{e.user}</h3><p>{e.score}</p><p>{e.date}</p></div>
+        })
+        : null}
     </div>
   )
 }
